033e9c0ce29bd5ff25be38306ea4e3e34a1d1758
くぁーりぃ <kaito6343@gmail.com>
2022-05-27 20:24:31 +0900

    add clay components crafting table recipes

--- src/main/java/clayium/loaders/recipe/ClayCraftingRecipeLoader.java
+++ src/main/java/clayium/loaders/recipe/ClayCraftingRecipeLoader.java
@@ -44,9 +44,6 @@ public class ClayCraftingRecipeLoader {
         //CLAY_SHOVEL
         ModHandler.addShapedRecipe("clay_shovel", CLAY_SHOVEL.getStackForm(Clay),
                 " P ", " S ", " S ", 'P', CLAY_PLATE, 'S', CLAY_STICK);
-        //LARGE_CLAY_BALL
-        ModHandler.addShapelessRecipe("clay_ball_large", CLAY_BALL_LARGE.getStackForm(),
-                Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL);
         //CLAY_MANUAL_MINER
         ModHandler.addShapedRecipe("clay_manual_miner", CLAY_MANUAL_MINER.getStackForm(),
                 "SGS", "GHG", "SGS", 'H', CLAY_MACHINE_HULL.getItemVariant(BlockClayMachineHull.BlockType.CLAY), 'S', CLAY_STICK, 'G', CLAY_GEAR);
@@ -59,5 +56,35 @@ public class ClayCraftingRecipeLoader {
         //CLAY_SPATULA_RAW
         ModHandler.addShapedRecipe("clay_spatula_raw", CLAY_SPATULA_RAW.getStackForm(),
                 "SB ", "   ", "   ", 'S', CLAY_STICK_SHORT, 'B', CLAY_BLADE);
+
+        //CLAY_COMPONENTS
+        ModHandler.addShapelessRecipe("clay_stick_short_1", CLAY_STICK_SHORT.getStackForm(2),
+                CLAY_STICK);
+        ModHandler.addShapelessRecipe("clay_stick_short_2", CLAY_STICK_SHORT.getStackForm(),
+                CLAY_RING_SMALL);
+        ModHandler.addShapelessRecipe("clay_ring", CLAY_RING.getStackForm(),
+                CLAY_CYLINDER);
+        ModHandler.addShapelessRecipe("clay_ring_small", CLAY_RING_SMALL.getStackForm(),
+                CLAY_STICK_SHORT);
+        ModHandler.addShapedRecipe("clay_gear", CLAY_GEAR.getStackForm(),
+                "SSS", "SRS", "SSS", 'S', CLAY_STICK_SHORT, 'R', CLAY_RING_SMALL);
+        ModHandler.addShapelessRecipe("clay_pipe", CLAY_PIPE.getStackForm(),
+                CLAY_PLATE);
+        ModHandler.addShapelessRecipe("clay_ball_large", CLAY_BALL_LARGE.getStackForm(),
+                Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL,Items.CLAY_BALL);
+        ModHandler.addShapedRecipe("clay_plate_large", CLAY_PLATE_LARGE.getStackForm(),
+                "PPP", "PPP", "PPP", 'P', CLAY_PLATE);
+        ModHandler.addShapedRecipe("clay_grinding_head", CLAY_GRINDING_HEAD.getStackForm(),
+                "NNN", "NRN", "NNN", 'N', CLAY_NEEDLE, 'R', CLAY_RING);
+        ModHandler.addShapedRecipe("clay_bearing", CLAY_BEARING.getStackForm(),
+                "CCC", "CRC", "CCC", 'C', Items.CLAY_BALL, 'R', CLAY_RING);
+        ModHandler.addShapedRecipe("clay_grinding_head", CLAY_GRINDING_HEAD.getStackForm(),
+                "NNN", "NRN", "NNN", 'N', CLAY_NEEDLE, 'R', CLAY_RING);
+        ModHandler.addShapedRecipe("clay_spindle", CLAY_SPINDLE.getStackForm(),
+                "RPR", "SBI", "RPR", 'R', CLAY_RING_SMALL, 'P', CLAY_PLATE,'S', CLAY_STICK,'B', CLAY_BEARING,'I', CLAY_RING);
+        ModHandler.addShapedRecipe("clay_cutting_head", CLAY_CUTTING_HEAD.getStackForm(),
+                "BBB", "BRB", "BBB", 'B', CLAY_BLADE, 'R', CLAY_RING);
+        ModHandler.addShapedRecipe("clay_wheel", CLAY_WHEEL.getStackForm(),
+                "PPP", "PRP", "PPP", 'P', CLAY_PLATE, 'R', CLAY_RING);
     }
 }
