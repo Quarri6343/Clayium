11c83ae4717c0fc6cc2763137836c80cf5ee7997
くぁーりぃ <kaito6343@gmail.com>
2022-06-11 15:32:20 +0900

    reconfigure t3 machines GUI

--- src/main/java/clayium/api/gui/ClayGuiTextures.java
+++ src/main/java/clayium/api/gui/ClayGuiTextures.java
@@ -5,6 +5,8 @@ import gregtech.api.gui.resources.TextureArea;
 public class ClayGuiTextures {
     //Buttons
     public static final TextureArea BUTTON_GEAR = TextureArea.fullImage("textures/gui/button_gear.png");
+    public static final TextureArea BUTTON_ITEM_INPUT = TextureArea.fullImage("textures/gui/button_item_input.png");
+    public static final TextureArea BUTTON_ITEM_OUTPUT = TextureArea.fullImage("textures/gui/button_item_output.png");
 
     //Background
 //    public static final TextureArea BACKGROUND_CLAY_WORKTABLE = TextureArea.fullImage("textures/gui/clayworktable.png");
